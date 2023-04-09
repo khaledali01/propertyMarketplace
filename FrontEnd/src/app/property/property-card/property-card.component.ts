275ce1f61d88b865d8e6c22094f75cfb076e084b
Khaled <khaled.ali.architect@gmail>
2023-04-09 12:57:40 +0200

    UI Foundation
    See: #2

--- FrontEnd/src/app/property/property-card/property-card.component.ts
+++ FrontEnd/src/app/property/property-card/property-card.component.ts
@@ -1,11 +1,10 @@
-import {Component} from '@angular/core'
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
-  templateUrl: "./property-card.component.html",
+  templateUrl: './property-card.component.html',
   styleUrls: ['./property-card.component.css'],
 })
-
 export class PropertyCardComponent {
-
+  @Input() property: any;
 }
