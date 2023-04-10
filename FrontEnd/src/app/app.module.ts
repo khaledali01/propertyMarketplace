67e4a44bd866db257412db71a06d8fdb627c6bc5
Khaled <khaled.ali.architect@gmail>
2023-04-10 03:46:53 +0200

    Adding Angular Routing
    See: #5

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PropertyService } from './services/property.service';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+
+const appRoutes: Routes = [
+  { path: '', component: PropertyListComponent },
+  { path: 'sell', component: AddPropertyComponent },
+];
 
 @NgModule({
   declarations: [
@@ -14,8 +21,9 @@ import { PropertyService } from './services/property.service';
     PropertyCardComponent,
     PropertyListComponent,
     NavBarComponent,
+    AddPropertyComponent,
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
   providers: [PropertyService],
   bootstrap: [AppComponent],
 })
